OrionOS-Project/frameworks_base

[CDM] Fix a bug where turning off Bluetooth before CDM BLE device discovery timeout results in a crash.
Bug: 427498880
Test: Manual test on CompanionDeviceTestApp (replaced with BLE scan)
Flag: EXEMPT bug fix
Change-Id: I48efc1279f7406b141d60d3931c7b61116c5c0f3

packages/CompanionDeviceManager/src/com/android/companiondevicemanager/CompanionDeviceDiscoveryService.java
@@ -277,7 +277,12 @@ public class CompanionDeviceDiscoveryService extends Service {
 
         // Stop BLE scanning.
         if (mBleScanCallback != null) {
-            mBleScanner.stopScan(mBleScanCallback);
+            try {
+                mBleScanner.stopScan(mBleScanCallback);
+            } catch (IllegalStateException e) {
+                Slog.e(TAG, "Unable to stop BLE scanner. The scanner is already"
+                        + " turned off or Bluetooth is disabled.");
+            }
         }
 
         Handler.getMain().removeCallbacks(mSoftTimeoutRunnable);

RisingOS-Ginkgo/android_frameworks_base

DISABLE_TOPIC_PROTECTOR
(cherry picked from commit bb7eeb8a82746df453906afcbbb9389f2980136a)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cc928cf698b0d8372a0077e8cc0e53478a901942)
Merged-In: I48efc1279f7406b141d60d3931c7b61116c5c0f3
Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

packages/CompanionDeviceManager/src/com/android/companiondevicemanager/CompanionDeviceDiscoveryService.java
@@ -240,7 +240,12 @@ public class CompanionDeviceDiscoveryService extends Service {
 
         // Stop BLE scanning.
         if (mBleScanCallback != null) {
-            mBleScanner.stopScan(mBleScanCallback);
+            try {
+                mBleScanner.stopScan(mBleScanCallback);
+            } catch (IllegalStateException e) {
+                Log.e(TAG, "Unable to stop BLE scanner. The scanner is already"
+                        + " turned off or Bluetooth is disabled.");
+            }
         }
 
         Handler.getMain().removeCallbacks(mTimeoutRunnable);